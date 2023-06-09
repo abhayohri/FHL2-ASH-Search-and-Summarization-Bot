a6264d8acb2a15f74c0ad9674b2d63e03c515a79
Cora Wu <corawu@microsoft.com>
2023-06-09 11:36:10 -0700

    Changing prompt examples to use actual events and tracking IDs for increased accuracy

--- SearchAndSummariztion/examples/ASH_Search_3.py
+++ SearchAndSummariztion/examples/ASH_Search_3.py
@@ -18,6 +18,7 @@ from configparser import ConfigParser
 from flask import Flask, request,  jsonify
 from flask_cors import CORS, cross_origin
 import openai
+import json
 
 parser = ConfigParser()
 parser.read('../secrets.cfg')
@@ -378,64 +379,49 @@ def get_chain_type_and_top_docs(question, tokens_limit,num_tokens,docs):
 # #### Search is complete time to Summarize the data:
 
 # %%
-COMBINE_QUESTION_PROMPT_TEMPLATE = """Use the following portion of a long document to see if any of the text is relevant to answer the question. 
-Return any relevant text in {language}.
-{context}
-Question: {question}
-Relevant text, if any, in {language}:"""
+def create_prompt_template():
+    template_start = """
+    These are examples of how you must provide the answer:
+    --> Beginning of examples
+    """
+    template_end = """
+    <-- End of examples
+    Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
+    If you don't know the answer, just say "Sorry, I couldn't find a clear answer for this.". Don't try to make up an answer.
+    ALWAYS return a "SOURCES" part in your answer.
+    Instead of using the word "Document" use "Tracking ID"
+    Respond in {language}.
+    =========
+    Chat History:
+    {chat_history}
+    =========
+    QUESTION: {question}
+    =========
+    {summaries}
+    =========
+    FINAL ANSWER IN {language}:"""
 
-COMBINE_QUESTION_PROMPT = PromptTemplate(
-    template=COMBINE_QUESTION_PROMPT_TEMPLATE, input_variables=["context", "question", "language"]
-)
+    examples_string = """"""
+    f = open("prompt_examples.json" , "rb" )
+    examples = json.load(f)
+    f.close()
 
+    for example in examples:
+        examples_string += "\n========="
+        examples_string += "\nQUESTION: " + example['question']
+        examples_string += "\n========="
+        for document in example['documents']:
+            examples_string += "\nContent: " + document['content']
+            examples_string += "\nSource: " + document['source']
+        examples_string += "\n========="
+        examples_string += "\nFINAL ANSWER IN English: " + example['answer']
+        examples_string += "\nSOURCES: " + example['answer_sources']
 
-COMBINE_PROMPT_TEMPLATE = """
-These are examples of how you must provide the answer:
---> Beginning of examples
-=========
-QUESTION: Which state/country's law governs the interpretation of the contract?
-=========
-Content: This Agreement is governed by English law and the parties submit to the exclusive jurisdiction of the English courts in  relation to any dispute (contractual or non-contractual) concerning this Agreement save that either party may apply to any court for an  injunction or other relief to protect its Intellectual Property Rights.
-Source: https://xxx.com/article1.pdf
-Content: No Waiver. Failure or delay in exercising any right or remedy under this Agreement shall not constitute a waiver of such (or any other)  right or remedy.\n\n11.7 Severability. The invalidity, illegality or unenforceability of any term (or part of a term) of this Agreement shall not affect the continuation  in force of the remainder of the term (if any) and this Agreement.\n\n11.8 No Agency. Except as expressly stated otherwise, nothing in this Agreement shall create an agency, partnership or joint venture of any  kind between the parties.\n\n11.9 No Third-Party Beneficiaries.
-Source: https://yyyy.com/article2.html
-Content: (b) if Google believes, in good faith, that the Distributor has violated or caused Google to violate any Anti-Bribery Laws (as  defined in Clause 8.5) or that such a violation is reasonably likely to occur,
-Source: https://yyyy.com/article3.csv
-Content: The terms of this Agreement shall be subject to the laws of Manchester, England, and any disputes arising from or relating to this Agreement shall be exclusively resolved by the courts of that state, except where either party may seek an injunction or other legal remedy to safeguard their Intellectual Property Rights.
-Source: https://ppp.com/article4.pdf
-=========
-FINAL ANSWER IN English: This Agreement is governed by English law, specifically the laws of Manchester, England.
-SOURCES: https://xxx.com/article1.pdf, https://ppp.com/article4.pdf
-=========
-QUESTION: What did the president say about Michael Jackson?
-=========
-Content: Madam Speaker, Madam Vice President, our First Lady and Second Gentleman. Members of Congress and the Cabinet. Justices of the Supreme Court. My fellow Americans.  \n\nLast year COVID-19 kept us apart. This year we are finally together again. \n\nTonight, we meet as Democrats Republicans and Independents. But most importantly as Americans. \n\nWith a duty to one another to the American people to the Constitution. \n\nAnd with an unwavering resolve that freedom will always triumph over tyranny. \n\nSix days ago, Russia’s Vladimir Putin sought to shake the foundations of the free world thinking he could make it bend to his menacing ways. But he badly miscalculated. \n\nHe thought he could roll into Ukraine and the world would roll over. Instead he met a wall of strength he never imagined. \n\nHe met the Ukrainian people. \n\nFrom President Zelenskyy to every Ukrainian, their fearlessness, their courage, their determination, inspires the world. \n\nGroups of citizens blocking tanks with their bodies. Everyone from students to retirees teachers turned soldiers defending their homeland.
-Source: https://fff.com/article23.pdf
-Content: And we won’t stop. \n\nWe have lost so much to COVID-19. Time with one another. And worst of all, so much loss of life. \n\nLet’s use this moment to reset. Let’s stop looking at COVID-19 as a partisan dividing line and see it for what it is: A God-awful disease.  \n\nLet’s stop seeing each other as enemies, and start seeing each other for who we really are: Fellow Americans.  \n\nWe can’t change how divided we’ve been. But we can change how we move forward—on COVID-19 and other issues we must face together. \n\nI recently visited the New York City Police Department days after the funerals of Officer Wilbert Mora and his partner, Officer Jason Rivera. \n\nThey were responding to a 9-1-1 call when a man shot and killed them with a stolen gun. \n\nOfficer Mora was 27 years old. \n\nOfficer Rivera was 22. \n\nBoth Dominican Americans who’d grown up on the same streets they later chose to patrol as police officers. \n\nI spoke with their families and told them that we are forever in debt for their sacrifice, and we will carry on their mission to restore the trust and safety every community deserves.
-Source: https://jjj.com/article56.pdf
-Content: And a proud Ukrainian people, who have known 30 years  of independence, have repeatedly shown that they will not tolerate anyone who tries to take their country backwards.  \n\nTo all Americans, I will be honest with you, as I’ve always promised. A Russian dictator, invading a foreign country, has costs around the world. \n\nAnd I’m taking robust action to make sure the pain of our sanctions  is targeted at Russia’s economy. And I will use every tool at our disposal to protect American businesses and consumers. \n\nTonight, I can announce that the United States has worked with 30 other countries to release 60 Million barrels of oil from reserves around the world.  \n\nAmerica will lead that effort, releasing 30 Million barrels from our own Strategic Petroleum Reserve. And we stand ready to do more if necessary, unified with our allies.  \n\nThese steps will help blunt gas prices here at home. And I know the news about what’s happening can seem alarming. \n\nBut I want you to know that we are going to be okay.
-Source: https://vvv.com/article145.pdf
-Content: More support for patients and families. \n\nTo get there, I call on Congress to fund ARPA-H, the Advanced Research Projects Agency for Health. \n\nIt’s based on DARPA—the Defense Department project that led to the Internet, GPS, and so much more.  \n\nARPA-H will have a singular purpose—to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more. \n\nA unity agenda for the nation. \n\nWe can do this. \n\nMy fellow Americans—tonight , we have gathered in a sacred space—the citadel of our democracy. \n\nIn this Capitol, generation after generation, Americans have debated great questions amid great strife, and have done great things. \n\nWe have fought for freedom, expanded liberty, defeated totalitarianism and terror. \n\nAnd built the strongest, freest, and most prosperous nation the world has ever known. \n\nNow is the hour. \n\nOur moment of responsibility. \n\nOur test of resolve and conscience, of history itself. \n\nIt is in this moment that our character is formed. Our purpose is found. Our future is forged. \n\nWell I know this nation.
-Source: https://uuu.com/article15.pdf
-=========
-FINAL ANSWER IN English: The president did not mention Michael Jackson.
-SOURCES: N/A
-<-- End of examples
-Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
-If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-ALWAYS return a "SOURCES" part in your answer.
-Instead of using the word "Document" use "Tracking ID"
-Respond in {language}.
-=========
-Chat History:
-{chat_history}
-=========
-QUESTION: {question}
-=========
-{summaries}
-=========
-FINAL ANSWER IN {language}:"""
+    print(examples_string)
 
+    return template_start + examples_string + template_end
+
+COMBINE_PROMPT_TEMPLATE = create_prompt_template()
 
 COMBINE_PROMPT = PromptTemplate(
     template=COMBINE_PROMPT_TEMPLATE, input_variables=["summaries", "question", "language", "chat_history"]
@@ -638,9 +624,6 @@ if mode == "Service":
         return response
     
     app.run()
-
-# %%
-
 
 # %% [markdown]
 # #######
